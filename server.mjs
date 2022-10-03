68d4ed6f6119e57be864dc4cf2192c296f7fe1c0
karikalanarun <arun.digital.karikalan1994@gmail.com>
2022-10-03 17:38:38 +0530

    initial commit

---
+++ server.mjs
@@ -0,0 +1,22 @@
+import { Server } from "socket.io";
+
+const io = new Server(process.env.PORT, {
+  // options
+});
+
+io.on("connection", (socket) => {
+  console.log("connected ...");
+  socket.on("start-game", (uuid) => {
+    socket.join(uuid);
+  });
+  socket.on("join-game", (uuid) => {
+    socket.join(uuid);
+    socket.to(uuid).emit("player-2-joined");
+  });
+  socket.on("move", ({ symbol, uuid, col, row }) => {
+    socket.to(uuid).emit("move", { symbol, uuid, col, row });
+  });
+  socket.on("game-over", (uuid, winningMsg) => {
+    socket.to(uuid).emit("game-over", winningMsg);
+  });
+});
